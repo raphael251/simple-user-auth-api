4aeb38d8bb51f965f7f14ad2300186252c1d7bb9
Raphael Passos <raphael251@hotmail.com>
2024-01-02 09:32:26 -0300

    chore: remove viper and add godotenv and env

--- configs/config.go
+++ configs/config.go
@@ -2,47 +2,44 @@ package configs
 
 import (
 	"fmt"
-	"log"
+	"os"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/jwtauth"
-	"github.com/spf13/viper"
+	"github.com/joho/godotenv"
 )
 
 type conf struct {
-	DBDriver           string `mapstructure:"DB_DRIVER"`
-	DBHost             string `mapstructure:"DB_HOST"`
-	DBPort             string `mapstructure:"DB_PORT"`
-	DBName             string `mapstructure:"DB_NAME"`
-	DBUser             string `mapstructure:"DB_USER"`
-	DBPassword         string `mapstructure:"DB_PASSWORD"`
+	DBDriver           string `mapstructure:"DB_DRIVER" env:"DB_DRIVER,required"`
+	DBHost             string `mapstructure:"DB_HOST" env:"DB_HOST,required"`
+	DBPort             string `mapstructure:"DB_PORT" env:"DB_PORT,required"`
+	DBName             string `mapstructure:"DB_NAME" env:"DB_NAME,required"`
+	DBUser             string `mapstructure:"DB_USER" env:"DB_USER,required"`
+	DBPassword         string `mapstructure:"DB_PASSWORD" env:"DB_PASSWORD,required"`
 	DBConnectionString string
-	ServerProtocol     string `mapstructure:"SERVER_PROTOCOL"`
-	ServerHost         string `mapstructure:"SERVER_HOST"`
-	ServerPort         string `mapstructure:"SERVER_PORT"`
-	JWTSecret          string `mapstructure:"JWT_SECRET"`
-	JWTExpiresIn       int    `mapstructure:"JWT_EXPIRES_IN"`
+	ServerProtocol     string `mapstructure:"SERVER_PROTOCOL" env:"SERVER_PROTOCOL,required"`
+	ServerHost         string `mapstructure:"SERVER_HOST" env:"SERVER_HOST,required"`
+	ServerPort         string `mapstructure:"SERVER_PORT" env:"SERVER_PORT,required"`
+	JWTSecret          string `mapstructure:"JWT_SECRET" env:"JWT_SECRET,required"`
+	JWTExpiresIn       int    `mapstructure:"JWT_EXPIRES_IN" env:"JWT_EXPIRES_IN,required"`
 	TokenAuthKey       *jwtauth.JWTAuth
 }
 
 func LoadConfig() (*conf, error) {
-	var configs *conf
+	configs := conf{}
 
-	viper.SetConfigName("app_config")
-	viper.SetConfigType("env")
-	viper.AddConfigPath(".")
-	viper.SetConfigFile(".env")
-	viper.AutomaticEnv()
+	/* godotenv will load the .env file variables into the os environment variables.
+	 * In docker environment we use environment variables directly, this is why we are not handling the error here.
+	 */
+	godotenv.Load()
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatal("Error trying to connect to the database")
-		panic(err)
-	}
-
-	err = viper.Unmarshal(&configs)
+	err := env.Parse(&configs)
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("aobaaaaaaaaaaaaa")
+	fmt.Println(os.Getenv("DB_NAME"))
 
 	configs.DBConnectionString = fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
@@ -51,5 +48,5 @@ func LoadConfig() (*conf, error) {
 
 	configs.TokenAuthKey = jwtauth.New("HS256", []byte(configs.JWTSecret), nil)
 
-	return configs, nil
+	return &configs, nil
 }
